518e5f32abdccdcb39f896923fb6d2e014fdfab6
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-07-12 20:54:36 -0500

    topics div in html, topics filled in js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
 
+
+
+// create an array of strings, each one releated to a topic that intrests me and save it to a variable called topics.
+var topics = ['mario', 'link', 'sonic', 'mega man', 'master chief', 'metal gear', 'kratos', 'monter hunter world', 'cloud strife', 'street fighter'];
+ 
+// create a for loop that appends a button for each string in the array
+for (var i = 0; i < topics.length; i++) {
+    var buttons = $('<button>'+ topics[i] + "</button>");
+    buttons.appendTo('#topics');
+    
+}
